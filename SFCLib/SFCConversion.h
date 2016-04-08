63bee96bda112aa6a679b5be74765a758be55b98
kwan2004 <kwan2004@163.com>
2016-04-08 22:54:17 +0200

    morton

--- SFCLib/SFCConversion.h
+++ SFCLib/SFCConversion.h
@@ -7,13 +7,42 @@ template< int nDims,  int nBits>
 class SFCConversion
 {
 public:
-	Point<long, nDims) ptInput;
-	Point<long, nBits) ptOutput;
+	Point<long, nDims) ptInput; //n*m
+	Point<long, nBits) ptOutput; //m*n
 
-	MortonEncode();
-	MortonDecode();
+	void MortonEncode(void) // from n*m to m*n
+	{
+		for (int i = 0; i < nBits; i++)//m
+		{
+			ptOutput[i] = 0;
+			long mask = 1 << (nBits - i);
+			for (int j = 0; j < nDims; j++) //get one bit from each dim
+			{
+				if (ptInput[j] & mask) // both 1
+					ptOutput[i] |= 1 << (nDims - j);// dim iteration
+			}//
+		}//m group
+	}
+	void MortonDecode(void)
+	{
+		for (int i = 0; i < nBits; i++)//m n-bits
+		{			
+			long ntemp = ptOutput[i]; // each row in  m n-bits
+			long mask = 1 << (nBits - i);
+			for (int j = 0; j < nDims; j++) 
+			{
+				if (ntemp & 1) //get the last bit from  each bits row 
+					ptInput[nDims-j-1] |= mask;
+				
+				ntemp >>= 1; //move forward one bit
+			}//
+		}//n dim
+	}
 
-	HilbertEncode();
-	HilbertDecode();
+	void HilbertEncode(void);
+	void HilbertDecode(void);
 };
+
+
+
 #endif
