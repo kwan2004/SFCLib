7ad12514c300a8e153fa424da73f15f15e38f0d7
yuyaojin <yuyaojin@whu.edu.cn>
2016-04-09 17:27:58 +0800

    the bitSequence2String function is added into OutputTransform class，but this function has something wrong and is unfinished。

--- SFCLib/OutputTransform.h
+++ SFCLib/OutputTransform.h
@@ -3,13 +3,14 @@
 #define OUTPUTTRANSFORM_H_
 #include "Point.h"
 #include <iostream>
+#include <bitset>
 using namespace std;
 
-enum StringType
+typedef enum
 {
 	Base32,
 	Base64,
-};
+} StringType;
 
 static const char* const BASE64_TABLE_E2 = "+/0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz=";
 class OutputTransform
@@ -36,13 +37,54 @@ public:
 		return result;
 	}
 
-	string bitSequence2String(Point<long> ptOutput,StringType)
+	string bitSequence2String(Point<long> ptOutput,StringType str_type)
 	{
 		//size=m;bitLength=n;
 		int size = ptOutput.returnSize();
+		//int bitLength = 0;
 		int bitLength = ptOutput.returnBitLength();
-	}
+		int totalBits=size*bitLength;
+		long *temp_result = new long[1];
+		int loopTimes = totalBits / 64;
+		int move = 0;
+		char *resultChar=NULL;
+		if ((totalBits <= 64))
+		{
+			*(temp_result) = bitSequence2Value(ptOutput);
+		}
+		else
+		{
+			int number = 64 / size;
+			for (int i = 0; i < loopTimes; i++)
+			{
+				for (int j = 0; j < size; j++)
+				{
+					move = size*(bitLength - j);
+					*(temp_result + i) |= (*(temp_result + i*number + j) << move);
+				}
+			}
+		}
+		for (int i = 0; i < loopTimes; i++)
+		{   
+			int result=0;
+			if (str_type == 1)
+			{   
+				int number = (sizeof(long) / 6);
+				for (int j = 0; j <number ; j++)
+				{
+					long mask = 1 <<((number-1)*6) ;
+					mask >> ((j - 1) * 6);
+					result = (*(temp_result + i)&mask);
+					result = result >> ((number - 1) * 6);
+					*(resultChar + i*6+j) = BASE64_TABLE_E2[result];
+				}
+			}
+		}
 
+		string resultStr(resultChar);
+		return resultStr;
+	}
+	
 
 };
 
