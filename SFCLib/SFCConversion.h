f0ebc75ad561897739e548216ef3e6b714166991
kwan2004 <kwan2004@163.com>
2016-04-11 12:20:27 +0200

    hilbert code

--- SFCLib/SFCConversion.h
+++ SFCLib/SFCConversion.h
@@ -2,13 +2,16 @@
 #ifndef SFCCONERSION_H_
 #define SFCCONERSION_H_
 
+#include <array>            // std::array
 #include "Point.h"
+
 
 template< int nDims,  int  mBits>
 class SFCConversion
 {
-
 private:
+	std::array< long, nDims > g_mask;
+
 	unsigned int calc_P3(int i, Point<long, mBits> H);
 	unsigned int calc_P2(unsigned int S);
 	unsigned int calc_J(unsigned int P);
@@ -19,6 +22,14 @@ private:
 public:
 	Point<long, nDims> ptCoord; //n*m
 	Point<long,  mBits> ptBits; //m*n
+
+	SFCConversion::SFCConversion()
+	{
+		for (int i = 0; i < nDims; i++)
+		{
+			g_mask[i] = 1 << i;
+		}
+	}
 
 	void MortonEncode(void); // from n*m coords to m*n bitsequence
 	void MortonDecode(void); // from m*n bitsequence to n*m coords
