9cafc47134650c582599519e82045db099a2316c
kwan2004 <kwan2004@163.com>
2016-04-17 08:43:31 +0200

    modify the outputransform class

--- SFCLib/OutputTransform.h
+++ SFCLib/OutputTransform.h
@@ -5,6 +5,7 @@
 #include "Point.h"
 #include <iostream>
 #include <bitset>
+
 using namespace std;
 
 typedef enum
@@ -31,8 +32,9 @@ public:
 	
 
 public:
-	long bitSequence2Value(Point<long, mBits> ptBits)
+	long BitSequence2Value(Point<long, mBits> ptBits)
 	{ 
+		if (mBits * nDims >= 64) return 0;
 		long result = 0;
 		for (int i = 0; i < mBits; i++)
 		{
@@ -41,119 +43,129 @@ public:
 		return result;
 	}
 
-	Point<long, mBits> Value2itSequence(long value)
+	Point<long, mBits> Value2BitSequence(long value)
 	{
 		Point<long, mBits> ptOutput;
+		if (mBits * nDims >= 64) return ptOutput;
 
-		int totalBits = nDims* mBits;
+		long mask = ((long)1 << nDims - 1);
 		for (int i = 0; i < mBits; i++)
-		{
-			temp_value = value;
-			for (int j = 0; j <nDims; j++)
-			{
-				move = nDims*(mBits - i);
-				long mask = ((long)1 << move - 1);
-				temp_value = temp_value & mask;
-				move = nDims*(mBits - i - 1);
-				temp_value >> move;
-				ptOutput[i] = temp;
-			}
-
+		{			
+			ptOutput[mBits - i - 1] = (value >> (i*nDims)) & mask;
 		}
+
 		return ptOutput;
 	}
 
-
-	string bitSequence2String(Point<long, mBits> ptBits, StringType str_type)
+	string BitSequence2String(Point<long, mBits> ptBits, StringType str_type)
 	{
-		//size=m;bitLength=n;
-		int size = ptBits.returnSize();
-		//int bitLength = 0;
-		int bitLength = ptBits.returnBitLength();
-		int totalBits=size*bitLength;
-		long *temp_result = new long[1];
-		int loopTimes = totalBits / 64;
-		int move = 0;
-		char *resultChar=NULL;
-		if ((totalBits <= 64))
+		int base = 0;
+		if (str_type == Base32) base = 5;
+		if (str_type == Base64) base = 6;
+
+		int ntotalbits = mBits * nDims;
+
+		//allocate the string space -----one more space for residual
+		int nstrlen = (ntotalbits % base) ? (ntotalbits / base + 1) : (ntotalbits / base);
+		char* szstr = new char[nstrlen + 1]; //last char for zero
+		memset(szstr, 0, nstrlen + 1);
+		int valid = strlen(szstr); //should be zero here
+
+		//find the least common multiple of base and  nDims( lcm <= base*n)
+		int nlcm = (base > nDims) ? base : nDims; 
+		while (1)                       /* Always true. */
 		{
-			*(temp_result) = bitSequence2Value(ptOutput);
-		}
-		else
-		{
-			int number = 64 / size;
-			for (int i = 0; i < loopTimes; i++)
+			if (nlcm % base == 0 && nlcm % nDims == 0)
 			{
-				for (int j = 0; j < number; j++)
-				{
-					move = bitLength*(number - j);
-					*(temp_result + i) |= (ptOutput[i*number + j]<< move);
-				}
+				break;          /* while loop terminates. */
+			}
+			++nlcm;
+		}
+
+		//////compare m*n with lcm (lcm = t*n), so totalbits(m*n) can be divided into m/t blocks (block=lcm=t*n)
+		int t = nlcm / nDims; //lcm is the bit number in one block; each block has t  nDims = t*n
+		int nblock = (mBits % t) ? (mBits / t + 1) : (mBits / t); // m/t+1 or m/t---one more space for residual
+				
+		array<long, nblock>; //long is 64 bits, base64=6 bits, enough for 10 more dims---to contain t*n (t<=base)		
+		for (int i = 0; i < nblock; i++)
+		{
+			///copy t*n to each block and get the decimal value
+			long nblkvalue = 0;
+			int nloops;
+			if ((mBits % t) && ( nblock-1 == i)) //handle the final block
+				nloops = mBits % t;
+			else
+				nloops = t;
+
+			for (int j = 0; j < nloops; j++)
+			{
+				nblkvalue |= ptBits[mBits - i*nblock - j] << (j*nDims);
+			}
+			
+			////change decimal value to character
+			long ncharnum = nloops*nDims / base;
+			unsigned int mask = ((unsigned int)1 << base) - 1;
+			int nidx = 0;
+			for (int j = 0; j < ncharnum; j++)
+			{
+				nidx = (nblkvalue >> (j*base)) & mask;
+				if (str_type == Base64)
+					*(szstr + (i*nblock + j)) = BASE64_TABLE_E2[nidx];
 			}
 		}
 
-		int baseNumber = 0;
-		if (str_type == 0)baseNumber = 5;
-		if (str_type == 0)baseNumber = 6;
-		int mod = 0;
-		int totalBit_eachResult = (64 / m)*bitLength;
-		int move = 0;
-		for (int i = 0; i < loopTimes; i++)
-		{   
-			//int result=0;
-			long each_Result = *(temp_result + i);
-			int times = totalBit_eachResult / baseNumber;
-			mod = total%baseNumber;
-			move = move + mod;
-			long temp = *(temp_result + i);
-			long *next_start = new long[1];
-			for (int j = 0; j < times; j++)
-			{
-				if (j == 0 )
-				{
-					long result = temp;
-					long mask = 1 << mod;
-					mask = mask - 1;
-				    *(next_start+i )= result&temp;
-					*(next_start + i) = *(next_start + i) << (baseNumber - mod);
-					temp >> move;
-					move = move + 6;
-				}
-				else
-				{
-					long result = temp;
-					long mask = 1 << Base;
-					mask = mask - 1;
-					result = result & mask;
-					*(resultChar + i * 6 + times - j) = BASE64_TABLE_E2[result];
-					if (i != 0&&(j==times-1))
-					{
-						result = *(next_start + i - 1)&result;
-						*(resultChar + i * 6 + times - j)=BASE64_TABLE_E2[result];
-					}
-					temp >> move;
-					move = move + 6;
-				}
+		string resultStr(szstr);
+		delete[] szstr;
 
-			}
-			totalBit_eachResult = totalBit_eachResult + mod;
-			/*	int number1 = (sizeof(long) / 6);
-				for (int j = 0; j <number1 ; j++)
-				{
-					long mask = 1 <<((number1-1)*6) ;
-					mask >> ((j - 1) * 6);
-					result = (*(temp_result + i)&mask);
-					result = result >> ((number1 - 1) * 6);
-					*(resultChar + i*6+j) = BASE64_TABLE_E2[result];
-				}*/
-
-
-		}
-
-		string resultStr(resultChar);
-		return resultStr;
+		return resultStr;	
 	}
-	
+
+	Point<long, mBits> BitSequence2String(string szCode, StringType str_type)
+	{
+		Point<long, mBits> ptBits;
+
+		int base = 0;
+		if (str_type == Base32) base = 5;
+		if (str_type == Base64) base = 6;
+
+		int ntotalbits = mBits * nDims;
+		int nstrlen = (ntotalbits % base) ? (ntotalbits / base + 1) : (ntotalbits / base);
+
+		//find the least common multiple of base and  nDims( lcm <= base*n)
+		int nlcm = (base > nDims) ? base : nDims;
+		while (1)                       /* Always true. */
+		{
+			if (nlcm % base == 0 && nlcm % nDims == 0)
+			{
+				break;          /* while loop terminates. */
+			}
+			++nlcm;
+		}
+
+		int nstride = nlcm / base; //how many characters will be processed once
+		unsigned int mask = ((unsigned int)1 << nDims) - 1;
+		for (int i = 0; i < nstrlen; i += nstride)
+		{
+			int nloops = nstride;
+			if (i + nstride > nstrlen)
+				nloops = nstrlen - i;
+			
+			//convert nloops character to a long value
+			int nvalue = 0;
+			for (int j = 0; j < nloops; j++)
+			{
+				nvalue |= szCode.c_str[i*nstride +j] << base;
+			}
+
+			int num = nloops * base / nDims;
+			for (int j = 0; j < num j++) //nloops character
+			{
+				ptBits[i*nstride + j] = (nvalue >> (nDims*j)) & mask;
+			}
+		}
+
+		return ptBits;
+	}	
 };
 
 #endif
